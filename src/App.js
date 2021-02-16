e17f045ee964a648248d579bee69e6d3669d2bc6
hylai2 <hsinyulai18@gmail.com>
2021-02-16 16:42:44 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="Frame1">
+        <div className="Title">CLIMATEHACKS</div>
+        <div className="SubTitle">Sat April 24-25 2021</div>
+        <div className="bluepart"></div>
+      </div>
+      <table className="WhiteFrame">
+        <th className="Category">Register</th>
+        <th className="Category">Volunteer</th>
+        <th className="Category">Sponsor</th>
+      </table>
+      <div className="Frame2">
+        <div className="bluepart"></div>
+        <div className="content1">What is Climate Hacks?</div>
+        <div className="content2">Organized by student organizers</div>
+        <div className="content3">Who Are MinorityProgrammers?</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
